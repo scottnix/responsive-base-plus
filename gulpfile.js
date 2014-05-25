fc6a7cce7cc0022de9f0a71c061606ea3e0cf9e0
Scott Nix <scott@scottnix.com>
2014-05-25 16:43:17 -0700

    Small clarity changes

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('styles', function() {
         compass: true,
         require: ['susy']
     }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest(''))
     .pipe(livereload(server))
     .pipe(notify({ message: 'Styles task complete' }));
