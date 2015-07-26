e1a2afacb627fac03368183f97dcc1e0be256fdb
Scott Nix <scott@scottnix.com>
2015-07-25 21:36:09 -0700

    Update NPM Packages and Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,36 @@
-// Load plugins
+// Load gulp and Plugins
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
-    notify = require('gulp-notify'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr'),
     server = lr();
 
 // Styles
 gulp.task('styles', function() {
-  return gulp.src('scss/style.scss')
-    .pipe(sass({
+  return sass('scss/style.scss', {
       style: 'expanded',
-      compass: true,
       require: ['susy']
+    })
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
     }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest(''))
     .pipe(livereload(server))
-    .pipe(notify({ message: 'Styles task complete' }));
 });
-
-// Default task
-gulp.task('default', ['styles', 'watch']);
 
 // Watch
 gulp.task('watch', function() {
   // Listen on port 35729
   server.listen(35729, function (err) {
     if (err) {
-    return console.log(err);
-  }
+      return console.log(err);
+    }
   // Watch .scss files
   gulp.watch('scss/**/*.scss', ['styles']);
   });
 });
 
+// Default Tasks
+gulp.task('default', ['styles', 'watch']);
